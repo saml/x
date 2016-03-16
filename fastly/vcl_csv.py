ae335368152fbe2c31c75cf50f06e29ba04f1617
saml <skynare@gmail.com>
2016-03-16 16:09:26 -0400

    full vcl template

--- fastly/vcl_csv.py
+++ fastly/vcl_csv.py
@@ -11,6 +11,7 @@ def to_vcl_str(spec: RowSpec) -> str:
         return '{{"""}} regsuball({},{{"""}},{{"\\""}}) {{"""}}'.format(spec.variable)
     return spec.variable
 
+
 Columns = [
     RowSpec('fastly_info.state', True),
     RowSpec('obj.hits', False),
@@ -32,8 +33,40 @@ Columns = [
     RowSpec('req.http.User-Agent', True),
 ]
 
+VCLTemplate = '''
+sub vcl_deliver {
+  if (req.http.Tmp-Set-Cookie) {
+    set resp.http.Set-Cookie = req.http.Tmp-Set-Cookie;
+  } 
+  set resp.http.X-FastlySessionID = req.http.X-FastlySessionID;
+
+#FASTLY deliver
+}
+
+sub vcl_log {
+  log {"%(log_name)s :: "} %(log_format)s
+}
+
+sub vcl_recv {
+  set req.http.X-FastlySessionID = regsub(req.http.Cookie, "^.*[; ]?fastlysid=([0-9a-z]+)[; ]?.*$", "\\1");
+  if (req.http.X-FastlySessionID ~ "[0-9a-z]+") {
+    set req.http.Tmp-Set-Cookie = req.http.Cookie;
+  } else {
+    set req.http.X-FastlySessionID = digest.hash_md5(now randomstr(32) client.ip);
+    set req.http.Tmp-Set-Cookie = if(req.http.Cookie, req.http.Cookie "; ", "")  "fastlysid="  req.http.X-FastlySessionID  "; Expires="  time.add(now, 87600h);
+  }
+  unset req.http.Cookie;
+ 
+#FASTLY recv
+}
+'''
+
+def write_fastly_vcl(log_format: str, log_name: str = 'syslog 7c8d3Wi3OpxNiRk5PkRF8A syslog') -> None:
+    print(VCLTemplate % {'log_format': log_format, 'log_name': log_name})    
+
+
 def main(columns: List[RowSpec]) -> None:
-    print(' "," '.join(map(to_vcl_str, columns)))
+    write_fastly_vcl(' "," '.join(map(to_vcl_str, columns)))
 
 if __name__ == '__main__':
     main(Columns)
