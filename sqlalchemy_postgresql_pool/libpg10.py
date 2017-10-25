6f380b5e49602edcc83cf3e96ba876cd2068cf2c
Sam Lee <slee@jwplayer.com>
2017-10-25 16:59:38 -0400

    asdf

--- sqlalchemy_postgresql_pool/libpg10.py
+++ sqlalchemy_postgresql_pool/libpg10.py
@@ -12,7 +12,7 @@ import sqlalchemy.exc
 Session = session_factory = sqlalchemy.orm.sessionmaker()
 
 
-def creator_lib10():
+def creator_libpg10():
     return psycopg2.connect(
         'user=postgres password=postgres host=localhost,localhost port=5454,5455 dbname=postgres connect_timeout=3')
 
@@ -37,7 +37,7 @@ def new_session():
 
 
 if __name__ == '__main__':
-    engine = sqlalchemy.create_engine('postgres://', creator=creator_manual_loop, echo=True)
+    engine = sqlalchemy.create_engine('postgres://', creator=creator_libpg10, echo=True)
     Session.configure(bind=engine)
     while True:
         try:
