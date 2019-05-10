845d3a3044faafe228289df64be8091b13e2da78
Sam Lee <slee@jwplayer.com>
2019-05-10 19:23:20 -0400

    asf

--- jsonserver/dir.go
+++ jsonserver/dir.go
@@ -1,15 +1,42 @@
 package main
 
-import "strings"
+import (
+	"path"
+	"strings"
+)
 
 // Split splits path into first segment and the rest.
-func Split(path string) (string, string) {
-	seg := strings.Trim(path, "/")
+func Split(resPath string) (string, string) {
+	seg := strings.Trim(resPath, "/")
 	idx := strings.IndexByte(seg, '/')
 	var rest string
 	if idx >= 0 {
 		seg = seg[:idx]
-		rest = path[idx+1:]
+		rest = resPath[idx+1:]
 	}
 	return seg, rest
+}
+
+// EnsureDirs creates given directory and all parent directories.
+func EnsureDirs(res Resource, dirPath string) (Resource, error) {
+	seg, rest := Split(dirPath)
+	if seg == "" {
+		return res, nil
+	}
+
+	child, err := res.Add(seg, nil)
+	if child != nil {
+		return EnsureDirs(child, rest)
+	}
+	return nil, err
+}
+
+// Add adds a new resource.
+func Add(res Resource, resPath string, data Data) (Resource, error) {
+	dirPath, name := path.Split(resPath)
+	dir, err := EnsureDirs(res, dirPath)
+	if err != nil {
+		return nil, err
+	}
+	return dir.Add(name, data)
 }
--- jsonserver/resource.go
+++ jsonserver/resource.go
@@ -5,14 +5,32 @@ import (
 	"reflect"
 )
 
+// Resource supports CRUD
+type Resource interface {
+	// Add adds a child of given name.
+	Add(name string, data Data) (Resource, error)
+
+	// Write overwrites the child of given name.
+	Write(name string, data Data) (Resource, error)
+
+	// Update updates part of the child.
+	Update(name string, patch Data) (Resource, error)
+
+	// Read reads the child.
+	Read(name string) (Resource, error)
+
+	// Del deletes the child.
+	Del(name string) (Resource, error)
+}
+
 // Data is JSON serializable.
 type Data map[string]interface{}
 
 // Children is child's name to data mapping.
-type Children map[string]*Resource
+type Children map[string]*InMemoryTree
 
-// Resource has JSON data and maybe children.
-type Resource struct {
+// InMemoryTree has JSON data and maybe children.
+type InMemoryTree struct {
 	Data     Data
 	Children Children
 }
@@ -28,51 +46,57 @@ var (
 	ErrNoChange = errors.New("ERR_NOCHANGE")
 )
 
-func (res *Resource) Has(name string) bool {
-	_, ok := res.Children[name]
-	return ok
+// Read reads child resource.
+func (res *InMemoryTree) Read(name string) (Resource, error) {
+	child, ok := res.Children[name]
+	if !ok {
+		return nil, ErrNotFound
+	}
+	return child, nil
 }
 
 // Add adds a child.
-func (res *Resource) Add(name string, data Data) error {
-	_, ok := res.Children[name]
+func (res *InMemoryTree) Add(name string, data Data) (Resource, error) {
+	child, ok := res.Children[name]
 	if ok {
-		return ErrExists
+		return child, ErrExists
 	}
-	res.Children[name] = &Resource{
+
+	res.Children[name] = &InMemoryTree{
 		Data: data,
 	}
-	return nil
+
+	return res.Read(name)
 }
 
 // Del deletes a child.
-func (res *Resource) Del(name string) error {
-	_, ok := res.Children[name]
+func (res *InMemoryTree) Del(name string) (Resource, error) {
+	child, ok := res.Children[name]
 	if !ok {
-		return ErrNotFound
+		return nil, ErrNotFound
 	}
 	delete(res.Children, name)
-	return nil
+	return child, nil
 }
 
 // Write overwrites child.
-func (res *Resource) Write(name string, data Data) error {
+func (res *InMemoryTree) Write(name string, data Data) (Resource, error) {
 	child, ok := res.Children[name]
 	if !ok {
-		return ErrNotFound
+		return nil, ErrNotFound
 	}
 	if reflect.DeepEqual(child.Data, data) {
-		return ErrNoChange
+		return child, ErrNoChange
 	}
 	child.Data = data
-	return nil
+	return child, nil
 }
 
 // Update updates child.
-func (res *Resource) Update(name string, patch Data) error {
+func (res *InMemoryTree) Update(name string, patch Data) (Resource, error) {
 	child, ok := res.Children[name]
 	if !ok {
-		return ErrNotFound
+		return nil, ErrNotFound
 	}
 	var newData Data
 	for k, v := range child.Data {
